c5485000dd5d46bc62b35d0c61dfb160d4ad3577
Mircea Bozianu <67138686+mirceabozianu@users.noreply.github.com>
2021-02-24 11:06:58 +0200

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Card from './components/Card';
+import App from './containers/App';
+import 'tachyons';
+import { robots } from './robots';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
